5b973ad0a4c1f1663ddea6799afe17802fa20f56
AmeenaAZIZ <ameena.aziz9@gmail.com>
2020-03-13 12:04:35 +0100

    Ajout des function dans methode get et post

--- app.js
+++ app.js
@@ -1,34 +1,66 @@
-const express = require("express");
 const request = require("request");
+const express = require("express");
 // Set up the express app
 const app = express();
+const https = require("https");
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // get all todos
-app.get("/histo", (req, res) => {
-  Promise.resolve()
+app.get("/", (req, res) => {
+  /* Promise.resolve()
     .then(() => {
-      return request.get({ url: "" });
+      return request.get({
+        url:
+          "https://modifymsgfunction20200312042701.azurewebsites.net/api/ModifyMessage?code=KL/UClMGc0CE55ofgVAGUhALAF5w7pcuozWQAQhyKq81Ihei/XHzPA=="
+      });
     })
     .then(body => {
       res.json(body);
     });
+*/
+
+  return request.get(
+    {
+      url:
+        "https://modifymsgfunction20200312042701.azurewebsites.net/api/ModifyMessage?code=KL/UClMGc0CE55ofgVAGUhALAF5w7pcuozWQAQhyKq81Ihei/XHzPA=="
+    },
+    function(error, response, body) {
+      console.log(body);
+      res.json(body);
+    }
+  );
 });
 
 app.post("/messages", (req, res) => {
-  console.log(req.body);
-  res.json(req.body.messages);
+  console.log(req.body.message);
 
-  Promise.resolve()
-    .then(() => {
-      return request.post({ url: "", data: res.json(req.body.messages) });
-    })
-    .then(body => {
+  return request.post(
+    {
+      url:
+        "https://modifymsgfunction20200312042701.azurewebsites.net/api/ModifyMessage?code=KL/UClMGc0CE55ofgVAGUhALAF5w7pcuozWQAQhyKq81Ihei/XHzPA==&name=" +
+        req.body.message
+    },
+    function(error, response, body) {
+      console.log(body);
       res.json(body);
-    });
+    }
+  );
+
+  /*
+  return request.post(
+    {
+      url: "https://devops-functions.azurewebsites.net/api/FormatMessage",
+      body: req.body,
+      json: true
+    },
+    function(error, response, body) {
+      console.log(body);
+      res.json(body);
+    }
+  );  */
 });
 
 const PORT = 5000;
