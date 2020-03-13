fdc9bc64482a716e9ed711b4a61c28e681fe852f
AmeenaAZIZ <ameena.aziz9@gmail.com>
2020-03-13 08:47:22 +0100

    methode get & post

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const express = require("express");
-const db = require("./db/db");
+const request = require("request");
 // Set up the express app
 const app = express();
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 // get all todos
 app.get("/histo", (req, res) => {
-  res.status(200).send({
-    success: "true"
-  });
+  Promise.resolve()
+    .then(() => {
+      return request.get({ url: "" });
+    })
+    .then(body => {
+      res.json(body);
+    });
 });
 
 app.post("/messages", (req, res) => {
   console.log(req.body);
   res.json(req.body.messages);
+
+  Promise.resolve()
+    .then(() => {
+      return request.post({ url: "", data: res.json(req.body.messages) });
+    })
+    .then(body => {
+      res.json(body);
+    });
 });
 
 const PORT = 5000;
